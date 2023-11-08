cb6abd54a296c91ff2621653005d04f5b116575c
Mikerophonic <micgcon@gmail.com>
2023-11-08 11:54:14 -0800

    add validation checks

--- Bakery/Models/Flavor.cs
+++ Bakery/Models/Flavor.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Bakery.Models
 {
  public class Flavor
  {
    public int FlavorId { get; set; }
+   [Required(ErrorMessage = "Please enter a description.")]
    public string Description { get; set; }
    public List<FlavorTreat> JoinEntities { get; set; } 
    public ApplicationUser User { get; set; }  
--- Bakery/Models/Treat.cs
+++ Bakery/Models/Treat.cs
@@ -1,10 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace Bakery.Models
 {
  public class Treat
  {
    public int TreatId { get; set; }
+   [Required(ErrorMessage = "Please enter a name.")]
    public string Name { get; set; }
    public List<FlavorTreat> JoinEntities { get; set; } 
    public ApplicationUser User { get; set; }  
